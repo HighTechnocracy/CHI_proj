9dbe9d79fdbf0b0c6b4a8c50d278ba922c1045c5
HighTechnocracy <jim@hightechnocracy.com>
2015-07-24 11:41:40 -0600

    took out region data because the important region is the state, with the county information visible for comparison

--- ui.R
+++ ui.R
@@ -9,24 +9,7 @@ shinyUI(fluidPage(
                                     label = "Choose a variable to display.",
                                     choices = c("Total Population", "Population per sq.mi.", "Under 18",
                                                 "Working Age", "Women of Child-bearing Age", "Over 65"),
-                                    selected = "Total Population"),
-                        
-                        selectInput("region", 
-                                    label = "Region of interest:",
-                                    choices = c("COLORADO", "Adams", "Alamosa", "Arapahoe", "Archuleta", 
-                                                 "Baca", "Bent", "Boulder", "Broomfield", "Chaffee", 
-                                                 "Cheyenne", "Clear Creek", "Conejos", "Costilla", 
-                                                 "Crowley", "Custer", "Delta", "Denver", "Dolores", 
-                                                 "Douglas", "Eagle", "El Paso", "Elbert", "Fremont", 
-                                                 "Garfield", "Gilpin", "Grand", "Gunnison", "Hinsdale", 
-                                                 "Huerfano", "Jackson", "Jefferson", "Kiowa", "Kit Carson", 
-                                                 "La Plata", "Lake", "Larimer", "Las Animas", "Lincoln", 
-                                                 "Logan", "Mesa", "Mineral", "Moffat", "Montezuma", 
-                                                 "Montrose", "Morgan", "Otero", "Ouray", "Park", "Phillips", 
-                                                 "Pitkin", "Prowers", "Pueblo", "Rio Blanco", "Rio Grande", 
-                                                 "Routt", "Saguache", "San Juan", "San Miguel", "Sedgwick", 
-                                                 "Summit", "Teller", "Washington", "Weld", "Yuma"),
-                                    selected = "COLORADO")
+                                    selected = "Total Population")
                         ),
                 
                 mainPanel(plotOutput("map"))
